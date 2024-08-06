732bda9082cb70954546e50967c7511583149ca7
rangasatriamukti <ranggastriamkti@gmail.com>
2024-08-06 09:44:21 +0700

    Add files via upload

---
+++ pertemuan 5/conditiont.py
@@ -0,0 +1,7 @@
+x = 2
+
+if x % 2 == 0:
+    print(x, "adalah bilangan genap")
+else:
+    print(x, "adalah bilangan ganjil")
+
---
+++ pertemuan 5/membership.py
@@ -0,0 +1,7 @@
+makanan = ("kue pancong", "nasi goreng", "burger", "lotek")
+
+print("apakah nasi uduk merupakan anggota?" "nasi uduk" in makanan)
+print("apakah burger merupakan anggota?" in makanan)
+
+print("apakah nasi uduk merupakan anggota?" "nasi uduk" not in makanan)
+print("apakah burger merupakan anggota?" not in makanan)
